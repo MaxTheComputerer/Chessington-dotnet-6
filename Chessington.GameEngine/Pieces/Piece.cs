99a1856f56fb30a62ca1b49d1e52aa30de6a3f70
Max Johnson <max.johnson@softwire.com>
2022-07-11 10:29:40 +0100

    Queen can move laterally and diagonally, and refactoring

--- Chessington.GameEngine/Pieces/Piece.cs
+++ Chessington.GameEngine/Pieces/Piece.cs
@@ -11,9 +11,59 @@ namespace Chessington.GameEngine.Pieces
         }
 
         public Player Player { get; private set; }
-        public int NumberOfMoves { get; set; }
+        protected int NumberOfMoves { get; private set; }
         
         public abstract IEnumerable<Square> GetAvailableMoves(Board board);
+
+        protected static IEnumerable<Square> GetLateralMoves(Square currentSquare)
+        {
+            var availableMoves = new List<Square>();
+
+            for (var i = 0; i < 8; i++)
+            {
+                availableMoves.Add(Square.At(currentSquare.Row, i));
+                availableMoves.Add(Square.At(i, currentSquare.Col));
+            }
+
+            //Get rid of our starting location.
+            availableMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
+
+            return availableMoves;
+        }
+
+        protected static IEnumerable<Square> GetDiagonalMoves(Square currentSquare)
+        {
+            var availableMoves = new List<Square>();
+
+            for (var i = 1; i < 8; i++)
+            {
+                // Down and right
+                if (currentSquare.Row + i < 8 && currentSquare.Col + i < 8)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col + i));
+                }
+                
+                // Up and left
+                if (currentSquare.Row - i >= 0 && currentSquare.Col - i >= 0)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row - i, currentSquare.Col - i));
+                }
+
+                // Down and right
+                if (currentSquare.Row + i < 8 && currentSquare.Col - i >= 0)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col - i));
+                }
+                
+                // Down and left
+                if (currentSquare.Row - i >= 0 && currentSquare.Col + i < 8)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row - i, currentSquare.Col + i));
+                }
+            }
+
+            return availableMoves;
+        }
 
         public void MoveTo(Board board, Square newSquare)
         {
--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -11,18 +11,7 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             var currentSquare = board.FindPiece(this);
-            var availableMoves = new List<Square>();
-
-            for (var i = 0; i < 8; i++)
-            {
-                availableMoves.Add(Square.At(currentSquare.Row, i));
-                availableMoves.Add(Square.At(i, currentSquare.Col));
-            }
-
-            //Get rid of our starting location.
-            availableMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
-
-            return availableMoves;
+            return GetLateralMoves(currentSquare);
         }
     }
 }
